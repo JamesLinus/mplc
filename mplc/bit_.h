8aa3aee61398f8330784fffd1dadad25676341df
Cthulhu <1948638989@qq.com>
2016-11-07 23:26:20 +0800

    categorize files;

---
+++ mplc/bit_.h
@@ -0,0 +1,6 @@
+#pragma once
+
+#include "bit\bit_and_.hpp"
+#include "bit\bit_not_.hpp"
+#include "bit\bit_or_.hpp"
+#include "bit\bit_xor_.hpp"
---
+++ mplc/operator_.hpp
@@ -0,0 +1,11 @@
+#pragma once
+
+#include "operator\operator_divide_.hpp"
+#include "operator\operator_equal_.hpp"
+#include "operator\operator_greater_.hpp"
+#include "operator\operator_greater_equal_.hpp"
+#include "operator\operator_less_.hpp"
+#include "operator\operator_less_equal_.hpp"
+#include "operator\operator_minus_.hpp"
+#include "operator\operator_multiply_.hpp"
+#include "operator\operator_plus_.hpp"
