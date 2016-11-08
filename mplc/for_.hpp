9d185105f7c0fb986d6270b09d0f2f6b8ef9849c
Cthulhu <1948638989@qq.com>
2016-11-08 08:05:49 +0800

    typedef while_ for_ :)

--- mplc/for_.hpp
+++ mplc/for_.hpp
@@ -1,23 +1,13 @@
 #pragma once
 
-namespace mplc {
-	template<int _range_start, int _range_end>
-	struct for_ {
-		static const int range_end = _range_end;
-		int range_start = _range_start;	//impure
-		template<typename _usr_meta_function_class>
-		for_(_usr_meta_function_class metafunc) {
-			for_impl(metafunc);
-		}
+#include "bool_.hpp"
 
-	private:
-		template<typename _usr_meta_function_class>
-		void for_impl(_usr_meta_function_class metafunc) {
-			if (range_end == range_start)//impure
-				return;					 //impure
-			metafunc(range_start);		 
-			range_start++;				 //impure
-			for_impl(metafunc);
-		}
+namespace mplc {
+	template<class ..._while_body>
+	struct for_ {
+		static const int value = for_<_while_body...>::value;
+	};
+	template<class ..._while_body> struct for_<false_, _while_body...> {
+		static const int value = 0;
 	};
 }
